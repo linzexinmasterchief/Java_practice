040122b1cbe575893ea805a64a0711a7269e3676
webDrag0n <linzexinmasterchief@gmail.com>
2020-07-23 16:13:56 +0100

    add hashset test

---
+++ src/HashTest/HashSet/HashSetTest.java
@@ -0,0 +1,12 @@
+package HashTest.HashSet;
+
+import java.util.HashSet;
+
+public class HashSetTest {
+    public static void main(String[] args) {
+        HashSet hashSet = new HashSet();
+        hashSet.add("a");
+        hashSet.add("b");
+        System.out.println(hashSet.toString());
+    }
+}
